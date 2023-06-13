edd9869fd1a3c08444a4b7386df9b8e43cc67946
Juan Diego Villar Hernandez <37917193+juandiego1999@users.noreply.github.com>
2023-06-13 10:37:23 -0400

    Update structure.sql

--- SQL/structure.sql
+++ SQL/structure.sql
@@ -1,78 +1,42 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 06-06-2023 a las 00:40:25
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Adminer 4.8.1 MySQL 5.5.5-10.3.38-MariaDB-0ubuntu0.20.04.1 dump
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `voting_form`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `candidates`
---
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
 
 DROP TABLE IF EXISTS `candidates`;
-CREATE TABLE IF NOT EXISTS `candidates` (
+CREATE TABLE `candidates` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `commune` int(11) NOT NULL,
   `name` text NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `commune` (`commune`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  KEY `commune` (`commune`),
+  CONSTRAINT `candidates_ibfk_1` FOREIGN KEY (`commune`) REFERENCES `communes` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=331 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `communes`
---
 
 DROP TABLE IF EXISTS `communes`;
-CREATE TABLE IF NOT EXISTS `communes` (
+CREATE TABLE `communes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `region` int(11) NOT NULL,
   `name` text NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `region` (`region`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  KEY `region` (`region`),
+  CONSTRAINT `communes_ibfk_1` FOREIGN KEY (`region`) REFERENCES `regions` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=331 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `regions`
---
 
 DROP TABLE IF EXISTS `regions`;
-CREATE TABLE IF NOT EXISTS `regions` (
+CREATE TABLE `regions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `votes`
---
 
 DROP TABLE IF EXISTS `votes`;
-CREATE TABLE IF NOT EXISTS `votes` (
-  `id` int(11) NOT NULL,
+CREATE TABLE `votes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `names` text NOT NULL,
   `alias` text NOT NULL,
   `rut` text NOT NULL,
@@ -81,36 +45,14 @@ CREATE TABLE IF NOT EXISTS `votes` (
   `commune` int(11) NOT NULL,
   `candidate` int(11) NOT NULL,
   `meet` text NOT NULL,
+  PRIMARY KEY (`id`),
   KEY `candidate` (`candidate`),
   KEY `commune` (`commune`),
-  KEY `region` (`region`)
+  KEY `region` (`region`),
+  CONSTRAINT `votes_ibfk_1` FOREIGN KEY (`candidate`) REFERENCES `candidates` (`id`),
+  CONSTRAINT `votes_ibfk_2` FOREIGN KEY (`commune`) REFERENCES `communes` (`id`),
+  CONSTRAINT `votes_ibfk_3` FOREIGN KEY (`region`) REFERENCES `regions` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Restricciones para tablas volcadas
---
 
---
--- Filtros para la tabla `candidates`
---
-ALTER TABLE `candidates`
-  ADD CONSTRAINT `candidates_ibfk_1` FOREIGN KEY (`commune`) REFERENCES `communes` (`id`);
-
---
--- Filtros para la tabla `communes`
---
-ALTER TABLE `communes`
-  ADD CONSTRAINT `communes_ibfk_1` FOREIGN KEY (`region`) REFERENCES `regions` (`id`);
-
---
--- Filtros para la tabla `votes`
---
-ALTER TABLE `votes`
-  ADD CONSTRAINT `votes_ibfk_1` FOREIGN KEY (`candidate`) REFERENCES `candidates` (`id`),
-  ADD CONSTRAINT `votes_ibfk_2` FOREIGN KEY (`commune`) REFERENCES `communes` (`id`),
-  ADD CONSTRAINT `votes_ibfk_3` FOREIGN KEY (`region`) REFERENCES `regions` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- 2023-06-13 14:34:32
